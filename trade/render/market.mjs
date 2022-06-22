0220a3b703df0048ac6c4fe4a121414e5e8bf10f
Thon Ly <thonly@heartbank.ceo>
2022-06-21 18:15:32 -0700

    add indexes

--- trade/render/market.mjs
+++ trade/render/market.mjs
@@ -21,6 +21,12 @@ export function renderMarket(market) {
 }
 
 export function renderMarkets(stocks) {
+    ['$DJI', '$SPX.X', '$COMPX'].forEach(index => {
+        document.getElementById(index).textContent = formatToDollars(stocks[index].lastPrice);
+        formatToDollar(document.getElementById(index + '-dollar'), stocks[index].netChange);
+        formatToPercent(document.getElementById(index + '-percent'), stocks[index].netPercentChangeInDouble);
+    });
+
     ['BRK.B', 'AAPL', 'SQ', 'ABNB'].forEach(stock => {
         document.getElementById(stock + '-bid-price').textContent = formatToDollars(stocks[stock].bidPrice);
         document.getElementById(stock + '-bid-size').textContent = formatToQuantity(stocks[stock].bidSize);
